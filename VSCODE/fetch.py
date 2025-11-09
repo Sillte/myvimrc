82175b2ef877e0d6e012387957d6d3544b88cc97
Sillte <innocent.konoha@gmail.com>
2025-11-09 15:46:18 +0900

    revise the crucial mistakes

--- VSCODE/fetch.py
+++ VSCODE/fetch.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from utils import get_vscode_user_dir, get_names, copy_with_backup
+from utils import get_vscode_user_dir, get_names, copy_with_backup, get_repo_folder
 
 
 _this_folder = Path(__file__).absolute().parent
@@ -9,7 +9,7 @@ def fetch_settings() -> None:
     for name in get_names():
         src_path = vscode_directory / name
         if src_path:
-            dst_path = _this_folder / name
+            dst_path = get_repo_folder() / name
             copy_with_backup(src_path, dst_path)
         else:
             print("`src_path` does not exist", src_path)
