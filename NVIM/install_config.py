7a35601224a1a1a8eb2dbe27a29949aec138e384
sillte <innocent.konoha@gmail.com>
2026-01-24 16:37:17 +0900

    [NVIM] update install_config

--- NVIM/install_config.py
+++ NVIM/install_config.py
@@ -6,7 +6,34 @@ _target_folder = _this_folder / "config_nvim"
 sys.path.append(_this_folder.as_posix())
 from utils_config import get_config_path, copyfiles_with_glob
 
+def _kill_imeoffhook():
+    import platform 
+    if platform.system() != "Windows":
+        return
+
+    import subprocess
+    from subprocess import PIPE, STDOUT
+
+    target_name = "IMEOFFHOOK_EXE.exe"
+
+    # プロセス一覧確認
+    result = subprocess.run(
+        f'chcp 65001 >nul & tasklist /FI "IMAGENAME eq {target_name}"',
+        text=True,
+        shell=True,
+        stdout=PIPE,
+        stderr=STDOUT,
+        encoding="utf-8"
+    )
+
+    if target_name in result.stdout:
+        print(f"Killing process {target_name}")
+        subprocess.run(f'taskkill /F /IM {target_name}', shell=True)
+    else:
+        print(f"No process named {target_name} is running")
+
 
 if __name__ == "__main__":
+    _kill_imeoffhook()
     config_path = get_config_path()
     copyfiles_with_glob(_target_folder, config_path, exclude=["site/**/*.lua", ".git/**/*", ".venv/**/*"])
