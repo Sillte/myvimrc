9ea1f116413fe5c35bde0bdde4fdaafdd3c46d8a
sillte <innocent.konoha@gmail.com>
2026-09-23 16:17:11 +0900

    WIP nvim files

--- NVIM/config_nvim/_plugins.vim
+++ NVIM/config_nvim/_plugins.vim
@@ -1,16 +1,7 @@
 let s:script_path = expand('<sfile>:p')
 let s:base_dir = fnamemodify(s:script_path, ':h')
-let s:plugin_dir = s:base_dir . '/_plugins' 
+let s:plugin_dir = s:base_dir . '/_plugins'
 
-echo s:plugin_dir 
-
-let s:patterns = [
-      \ s:plugin_dir . '/*.vim',
-      \ s:plugin_dir . '/*/*.vim',
-      \ ]
-
-for pattern in s:patterns
-  for file in split(glob(pattern), '\n')
-      execute 'source' fnameescape(file)
-  endfor
+for file in sort(globpath(s:plugin_dir, '**/*.vim', 0, 1))
+  execute 'source' fnameescape(file)
 endfor
--- NVIM/config_nvim/init.lua
+++ NVIM/config_nvim/init.lua
@@ -1,36 +1,46 @@
-local site_dir = vim.fn.stdpath("config") .. "/site"
-local pre_site_dir = site_dir .. "/pre"
-
+local config_dir = vim.fn.stdpath("config")
+local site_dir = config_dir .. "/site"
 local lua_loader = require("luafiles_loader")
-lua_loader.load_lua_files_from(pre_site_dir)
 
-require("config.options")
-require("config.keymaps")
-require("config.autocmds")
-require("config.extras")
-
-vim.g.python3_host_prog = require("python_venv").get_python3_host_prog()
-
-local lazypath = require("_install_lazy").lazypath
-vim.opt.rtp:prepend(lazypath)
-
-require("pytoy_reboot")
-
-if vim.g.vscode then
-    require("vscode_config")
-    require("vscode_lazy_setup")
-    if vim.g.pytoy_reboot ~= nil then
-        vim.opt.rtp:prepend(vim.g.pytoy_reboot)
-    end
-else
-    --Plugin manager.
-    require("lazy_setup")
+local function load_core_config()
+    require("config.options")
+    require("config.keymaps")
+    require("config.autocmds")
+    require("config.extras")
 end
 
--- Handling of `_vim` files.
-local script_path = debug.getinfo(1, "S").source:sub(2)
-vim.cmd("source " .. vim.fn.fnamemodify(script_path, ':h') .. '/_plugins.vim')
+local function setup_python_host()
+    vim.g.python3_host_prog = require("python_venv").get_python3_host_prog()
+end
 
+local function setup_lazy()
+    local lazypath = require("_install_lazy").lazypath
+    vim.opt.rtp:prepend(lazypath)
+end
 
-local post_site_dir = site_dir .. "/post"
-lua_loader.load_lua_files_from(post_site_dir)
+local function setup_plugin_manager()
+    require("pytoy_reboot")
+
+    if vim.g.vscode then
+        require("vscode_config")
+        require("vscode_lazy_setup")
+        if vim.g.pytoy_reboot ~= nil then
+            vim.opt.rtp:prepend(vim.g.pytoy_reboot)
+        end
+    else
+        require("lazy_setup")
+    end
+end
+
+local function source_legacy_plugins()
+    local init_path = debug.getinfo(1, "S").source:sub(2)
+    local plugin_file = vim.fs.joinpath(vim.fn.fnamemodify(init_path, ":h"), "_plugins.vim")
+    vim.cmd("source " .. vim.fn.fnameescape(plugin_file))
+end
+lua_loader.load_lua_files_from(site_dir .. "/pre")
+load_core_config()
+setup_python_host()
+setup_lazy()
+setup_plugin_manager()
+source_legacy_plugins()
+lua_loader.load_lua_files_from(site_dir .. "/post")
